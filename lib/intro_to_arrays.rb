0ad305fa57de740b91af1bc166d31027dbf36416
stuypend <andre@stuypend.com>
2021-02-22 10:58:09 -0500

    commit tree eb4cd3620db65e35e1444e70af5f824dec65a2e8

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def  instantiate_new_array
+  arr = []
+end
+
+def array_with_two_elements
+  arr = [2,3]
+end
+
+def first_element(arr)
+  arr[0]
+end
+
+def third_element(arr)
+  arr[2]
+end
+
+def last_element(arr)
+  return arr[-1]
+end
+
+def first_element_with_array_methods(arr)
+  arr.first
+end
+
+def last_element_with_array_methods(r)
+  r.last
+end
+
+def length_of_array(r)
+  r.size
+end
